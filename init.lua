e119b3a1245307620eb68d1f5edc3d1f55277f61
Allen-Mikhailov <mikhailovallen@gmail.com>
2025-12-08 22:00:26 -0600

    Added line numbers and attempted to fix some bugs with config

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require("config.lazy")
 
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "c", "javascript", "typescript", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+  ensure_installed = { "c", "javascript", "typescript", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -68,83 +68,35 @@ require("neo-tree").setup({
 
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function()
+		-- Status Col
+		vim.cmd("set number")
+		vim.cmd("set signcolumn=auto")
+		vim.cmd("set foldcolumn=0")
+
+		-- Open Neodtree
 		vim.cmd("Neotree")
+
+		-- Set Tabs
 		vim.cmd("set tabstop=4")
 		vim.cmd("set shiftwidth=4")
+
+		
 	end,
+})
+
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "neo-tree",
+	callback = function()
+		-- vim.opt_local.foldcolumn = "0"
+		-- vim.opt_local.number = false
+		-- vim.opt_local.relativenumber = false
+		-- vim.opt_local.signcolumn = "no"
+	end
 })
 
 -- Reserve a space in the gutter
 -- This will avoid an annoying layout shift in the screen
 vim.opt.signcolumn = 'yes'
 
--- Add cmp_nvim_lsp capabilities settings to lspconfig
--- This should be executed before you configure any language server
-local lspconfig_defaults = require('lspconfig').util.default_config
-lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-  'force',
-  lspconfig_defaults.capabilities,
-  require('cmp_nvim_lsp').default_capabilities()
-)
-
--- This is where you enable features that only work
--- if there is a language server active in the file
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function(event)
-    local opts = {buffer = event.buf}
-
-    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-  end,
-})
-
-require('lspconfig').clangd.setup({})
-
-vim.lsp.enable('clangd')
-
-local cmp = require('cmp')
-
-cmp.setup({
-  sources = {
-    {name = 'nvim_lsp'},
-	{ name = "buffer" },
-  { name = "path" },
-  },
-  mapping = cmp.mapping.preset.insert({
-    -- Navigate between completion items
-    ['<C-p>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-    ['<C-n>'] = cmp.mapping.select_next_item({behavior = 'select'}),
-
-    -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-
-    -- Ctrl+Space to trigger completion menu
-    ['<C-Space>'] = cmp.mapping.complete(),
-
-    -- Scroll up and down in the completion documentation
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-  }),
-  snippet = {
-    expand = function(args)
-      vim.snippet.expand(args.body)
-    end,
-  },
-})
-
--- Setup LSP capabilities
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
--- Enable clangd for C/C++
-require("lspconfig").clangd.setup({
-capabilities = capabilities,
-})
+require("autocomplete")
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -2,4 +2,16 @@ return {
 	{'neovim/nvim-lspconfig', tag = 'v1.8.0', pin = true},
 	{'hrsh7th/cmp-nvim-lsp'},
 	{'hrsh7th/nvim-cmp'},
+	
+	-- Type checking for typescript
+	{
+	  "jose-elias-alvarez/typescript.nvim",
+	  config = function()
+		require("typescript").setup({
+		  server = {
+			capabilities = require("cmp_nvim_lsp").default_capabilities(),
+		  },
+		})
+	  end,
+	}
 }
